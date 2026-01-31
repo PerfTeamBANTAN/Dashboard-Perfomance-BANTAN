fc0dacd17f87bf334af12f56c05b91314288cd5b
PerfTeamBANTAN <terbaiktangerang@gmail.com>
2026-01-31 18:59:03 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,14 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbyj_xhjKdCzPsQTOQENE3AgdZ4y0NGiGQ5D7ogVs9dA6LCQBvFi8ccMH4MLifajS9uQ/exec";
+
+function sendRequest(page, action, data){
+  return fetch(API_URL,{
+    method:"POST",
+    body: JSON.stringify({
+      page: page,
+      action: action,
+      ...data
+    })
+  })
+  .then(res => res.json());
+}
 
